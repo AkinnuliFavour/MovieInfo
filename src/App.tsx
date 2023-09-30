3482e2c0f9b221b191b8e23172845b8a35e34b8f
Gandort <oluwasemiloreakinnuli@gmail.com>
2023-09-30 11:18:57 +0100

    fix routing issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 // import { useState } from 'react'
 import Home from "./pages/home/Home"
 import SingleMovie from "./pages/single-movie/SingleMovie"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { HashRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie-details/:id" element={<SingleMovie />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
