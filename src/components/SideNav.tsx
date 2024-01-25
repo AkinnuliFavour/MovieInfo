7b7c9f2c6b80cfb3c5c47c147377b23c71e9180b
Gandort <oluwasemiloreakinnuli@gmail.com>
2024-01-25 11:03:16 +0100

    Refactor routing in App component and add Movies page

---
+++ src/components/SideNav.tsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom'
+import Logo from '../assets/logo-dark-text.png'
+import { linkStyle } from '../lib/utils'
+
+const SideNav = () => {
+  return (
+    <nav className="border-r min-h-screen border-black border-opacity-30 rounded-r-3xl w-1/6 py-10 hidden lg:flex flex-col justify-between fixed top-0 left-0">
+        <div className='px-[20px]'>
+          <img src={Logo} alt="" className='w-[186px] h-[50px]'/>
+        </div>
+        <ul className='h-[300px] w-full flex flex-col justify-between'>
+            <li className=''><Link to='/' className={linkStyle}>Home</Link></li>
+            <li><Link to='' className={linkStyle}>Movies</Link></li>
+            <li><Link to='' className={linkStyle}>TV series</Link></li>
+            <li><Link to='' className={linkStyle}>Upcoming</Link></li>
+        </ul>
+        <div>
+            <p></p>
+        </div>
+        <p className='px-[20px]'>Log out</p>
+    </nav>
+  )
+}
+
+export default SideNav
