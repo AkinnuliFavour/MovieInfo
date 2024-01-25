a13649eaa92f02294cdfad662810aab2fe051db6
Gandort <oluwasemiloreakinnuli@gmail.com>
2024-01-25 13:29:44 +0100

    Update link to movies in SideNav component

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -10,7 +10,7 @@ const SideNav = () => {
         </div>
         <ul className='h-[300px] w-full flex flex-col justify-between'>
             <li className=''><Link to='/' className={linkStyle}>Home</Link></li>
-            <li><Link to='' className={linkStyle}>Movies</Link></li>
+            <li><Link to='/movies' className={linkStyle}>Movies</Link></li>
             <li><Link to='' className={linkStyle}>TV series</Link></li>
             <li><Link to='' className={linkStyle}>Upcoming</Link></li>
         </ul>
