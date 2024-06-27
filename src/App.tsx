01fd22e752bb8e3f22751fbcf19dd38a2a415a55
Akinnuli Favour <oluwasemiloreakinnuli@gmail.com>
2024-06-27 11:12:59 +0100

    Added new routes and imports for react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 // import { useState } from 'react'
-import Home from "./pages/home/Home"
-import TopRated from "./pages/top-rated/TopRated"
-import SingleMovie from "./pages/single-movie/SingleMovie"
-import NowPlaying from "./pages/now-playing/NowPlaying"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Upcoming from "./pages/upcoming/Upcoming"
-import Search from "./pages/search/Search"
+import Home from "./pages/home/Home";
+import TopRated from "./pages/top-rated/TopRated";
+import SingleMovie from "./pages/single-movie/SingleMovie";
+import NowPlaying from "./pages/now-playing/NowPlaying";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+import Upcoming from "./pages/upcoming/Upcoming";
+import Search from "./pages/search/Search";
+import Signup from "./pages/auth/SignUp";
+import Login from "./pages/auth/Login";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/top-rated" element={<TopRated />} />
-        <Route path="/now-playing" element={<NowPlaying />} />
-        <Route path="/upcoming" element={<Upcoming />} />
-        <Route path="/search/:title" element={<Search />} />
-        <Route path="/movie-details/:id" element={<SingleMovie />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/top-rated" element={<TopRated />} />
+          <Route path="/now-playing" element={<NowPlaying />} />
+          <Route path="/upcoming" element={<Upcoming />} />
+          <Route path="/search/:title" element={<Search />} />
+          <Route path="/movie-details/:id" element={<SingleMovie />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
