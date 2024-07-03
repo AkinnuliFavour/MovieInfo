318dcbd477494a87c709c0964c21aef94d7e674e
Akinnuli Favour <oluwasemiloreakinnuli@gmail.com>
2024-07-03 16:16:08 +0100

    fix: Changed dashboard Nav component link from p tag to react-router-dom Link

--- src/pages/dashboard/components/Nav.tsx
+++ src/pages/dashboard/components/Nav.tsx
@@ -1,4 +1,5 @@
 import Logo from "../../../assets/logo-dark-text.png";
+import { Link } from "react-router-dom";
 
 const Nav = ({ showMenu }: { showMenu: boolean }) => {
   return (
@@ -10,41 +11,41 @@ const Nav = ({ showMenu }: { showMenu: boolean }) => {
       <img src={Logo} alt="" className="mt-5 ml-6 hidden lg:block" />
 
       <ul className="h-full flex flex-col justify-around">
-        <li className="px-6">
+        <Link to="/dashboard" className="px-6">
           {/* <img src="../../assets/Icons/Home.svg" alt="" /> */}
           <p className="py-4 rounded-md hover:text-center hover:bg-rose-600 hover:text-white hover:cursor-pointer">
             Dashboard
           </p>
-        </li>
-        <li className="px-6">
+        </Link>
+        <Link to="/dashboard/watchlist" className="px-6">
           <img src="../../assets/Icons/Calendar.svg" alt="" />
           <p className="py-4 rounded-md hover:text-center hover:bg-rose-600 hover:text-white hover:cursor-pointer">
             Watchlist
           </p>
-        </li>
-        <li className="px-6">
+        </Link>
+        <Link to="/dashboard/forums" className="px-6">
           {/* <img src="../../assets/Icons/Settings.svg" alt="" /> */}
           <p className="py-4 rounded-md hover:text-center hover:bg-rose-600 hover:text-white hover:cursor-pointer">
             Forums
           </p>
-        </li>
-        <li className="px-6">
+        </Link>
+        <Link to="/dashboard/news" className="px-6">
           {/* <img src="../../assets/Icons/Settings.svg" alt="" /> */}
           <p className="py-4 rounded-md hover:text-center hover:bg-rose-600 hover:text-white hover:cursor-pointer">
             News
           </p>
-        </li>
-        <li className="px-6">
+        </Link>
+        <Link to="/dashboard/streams" className="px-6">
           {/* <img src="../../assets/Icons/Settings.svg" alt="" /> */}
           <p className="py-4 rounded-md hover:text-center hover:bg-rose-600 hover:text-white hover:cursor-pointer">
             Stream
           </p>
-        </li>
-        <li className="px-6">
+        </Link>
+        <Link to="/dashboard/settings" className="px-6">
           <p className="py-4 rounded-md hover:text-center hover:bg-rose-600 hover:text-white hover:cursor-pointer">
             Settings
           </p>
-        </li>
+        </Link>
         <li>{/* <p>Settings</p> */}</li>
       </ul>
     </nav>
