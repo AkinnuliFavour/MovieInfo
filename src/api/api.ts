47770f23c751933ce6cc4aad67fe0f474d270d3a
AkinnuliFavour <oluwasemiloreakinnuli@gmail.com>
2025-12-08 03:51:00 +0100

    feat: Update API base URL to production server; switch from localhost to live endpoint

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001", // adjust this to your API's URL
+  baseURL: "https://movieinfo-server-1.vercel.app", // adjust this to your API's URL
   withCredentials: true, // This is the key setting
 });
 
